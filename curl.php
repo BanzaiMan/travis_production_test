0c5924ac1988d5279e2aa5d34fe816612434548f
Hiro Asari <asari.ruby@gmail.com>
2016-01-08 11:38:22 -0500

    Test https connexion

---
+++ curl.php
@@ -0,0 +1,12 @@
+<?php
+
+$ch = curl_init("https://google.com/");
+$fp = fopen("example_homepage.txt", "w");
+
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+curl_exec($ch);
+curl_close($ch);
+fclose($fp);
+?>
