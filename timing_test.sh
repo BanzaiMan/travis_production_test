3f752a65b36400df99a1cc0b0453f27983812bd0
Hiro Asari <asari.ruby@gmail.com>
2016-09-22 12:01:42 -0400

    Test timing markers in a script

---
+++ timing_test.sh
@@ -0,0 +1,17 @@
+travis_time_start() {
+  travis_timer_id=$(printf %08x $(( RANDOM * RANDOM )))
+  travis_start_time=$(travis_nanoseconds)
+  echo -en "travis_time:start:$travis_timer_id\r${ANSI_CLEAR}"
+}
+
+travis_time_finish() {
+  local result=$?
+  travis_end_time=$(travis_nanoseconds)
+  local duration=$(($travis_end_time-$travis_start_time))
+  echo -en "\ntravis_time:end:$travis_timer_id:start=$travis_start_time,finish=$travis_end_time,duration=$duration\r${ANSI_CLEAR}"
+  return $result
+}
+
+travis_time_start
+sleep 5
+travis_time_finish
