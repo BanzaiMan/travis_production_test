402b5b90da352a55f7eb88b3b720a6590c075325
Hiro Asari <asari.ruby@gmail.com>
2017-01-05 08:54:56 -0500

    Test some ANSI sequences

--- echo.sh
+++ echo.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
 ANSI_RESET="\033[0m"
+ANSI_WHITE="\033[37m"
 
  echo -e "${ANSI_RESET}IS THIS LINE VISIBLE?"
- echo -e "${ANSI_RESET}\nHOW ABOUT THIS ONE?"
+ echo -e "${ANSI_RESET}\n${ANSI_WHITE}HOW ABOUT THIS ONE?"
 
